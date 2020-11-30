bf940b55e92f635e68b544e76951a8a206394708
Auroka <auroka@163.com>
2020-11-30 09:49:42 +0800

    fix: 样式修改

--- src/views/menu/menu2/index.js
+++ src/views/menu/menu2/index.js
@@ -3,13 +3,15 @@
  * @Author: lxd
  * @Date: 2020-11-25 16:41:23
  * @LastEditors: lxd
- * @LastEditTime: 2020-11-25 16:50:22
+ * @LastEditTime: 2020-11-30 09:48:58
  */
 function Menu2(props) {
   return (
-    <div>
+    <div style={{ padding: '20px', background: '#fff' }}>
       menu2
-      <div>子菜单：{props.children}</div>
+      <div style={{ margin: '20px', padding: '20px', background: '#ccc' }}>
+        {props.children}
+      </div>
     </div>
   )
 }
